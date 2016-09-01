5b6ce024111b9bb8fba92573d6d1c532bcfd7e36
NatureZhang <895234387@qq.com>
2016-09-01 23:51:59 +0800

    创建工程
    创建工程，用于展示fir上应用

---
+++ FirTool/FirTool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FirTool
+//
+//  Created by 张东 on 16/9/1.
+//  Copyright © 2016年 __Nature__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FirTool/FirTool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirTool
+//
+//  Created by 张东 on 16/9/1.
+//  Copyright © 2016年 __Nature__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
